e7e667f4f7886772e077a938977e85e124761c8a
Jason Hernandez <jasonhr781@gmail.com>
2018-11-02 14:46:50 -0400

    Code

--- src/ProblemSet4.java
+++ src/ProblemSet4.java
@@ -1,26 +1,39 @@
-/**
- * INSTRUCTIONS.
- * 
- * Welcome to Problem Set 4, where we'll dive into writing our own methods with
- * parameters and return values. The focus, this time, will be on the built-in @String
- * and @Math classes.
- * 
- * You'll note that most of the methods have not been included. Instead, you'll see
- * specifications pertaining to their signatures (i.e., the method name, as well as
- * its return type and parameter list). You'll need to be sure to meet these
- * specifications exactly.
- * 
- * As always, please be mindful of your output (in this case, returned values rather
- * than printed text). Your solutions will be tested against a set of expected
- * values, and the grading script will expect an exact match.
- * 
- * This problem set contains 10 exercises worth 50 points. It is due no later than
- * 11:59pm on Sunday, November 4, 2018. Good luck! 
- */
-
 public class ProblemSet4 {
 	
 	public static void main(String[] args) {
-		
+		ProblemSet4 ps4 = new ProblemSet4();
+		ps4.surroundMe("<<>>", "abc");
+		//ps4.endsMeet("basl", 3);
+		//ps4.middleMan("programming");
+		//ps4.doubleVision("qwerty");
+		//ps4.centered("programming", "ram");
+		//ps4.upOrDown(12.2, 'c');
+		//ps4.countMe("these are just some sample words", 'e');
+		//ps4.isNotEqual("is not is not");
 	}
+	public String surroundMe(String in, String out) {
+		int lengthIn = in.length();
+		int lengthOut = out.length();
+		if (in.equals(null) || out.equals(null)) {
+			return null;
+		}
+		else if (lengthIn != 3) {
+			return null;
+		}
+		else if (lengthOut != 4) {
+			return null;
+		}
+		else {
+		String outFirst = out.substring(0, 2);
+		String outLast = out.substring(2,4);
+		String lastString = outFirst + in + outLast;
+		System.out.println(lastString);
+		return (lastString);
+		
+		}
+	}
+	
+	/*public int endsMeet(String str, int n) {
+		return n;
+	}*/
 }
